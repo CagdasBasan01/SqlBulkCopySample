f6423df72bf36a2691b10475e710c7f9efde17c8
Çağdaş Başan <cagdasbasan@yahoo.com>
2023-01-14 22:37:25 +0300

    create table added

--- SqlBulkCopySample/Program.cs
+++ SqlBulkCopySample/Program.cs
@@ -59,5 +59,29 @@ namespace SqlBulkCopySample
             }
             return samples;
         }
+
+            //        USE[Sample]
+            //GO
+
+            ///****** Object:  Table [dbo].[SampleTable]    Script Date: 14.01.2023 22:36:07 ******/
+            //SET ANSI_NULLS ON
+            //GO
+
+            //SET QUOTED_IDENTIFIER ON
+            //GO
+
+            //CREATE TABLE[dbo].[SampleTable]
+            //        (
+
+            //    [Id][int] IDENTITY(1,1) NOT NULL,
+
+            //    [Name] [nvarchar] (50) NULL,
+            //	[Description][nvarchar] (50) NULL,
+            // CONSTRAINT[PK_SampleTable] PRIMARY KEY CLUSTERED
+            //(
+            //    [Id] ASC
+            //)WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON[PRIMARY]
+            //) ON[PRIMARY]
+            //GO
     }
 }
